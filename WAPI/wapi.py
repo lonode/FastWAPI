820200796a0939b21d4ca8b60388634447ddf249
lonode <lonode-git@proton.me>
2023-06-15 11:32:33 +0200

    changed author

---
+++ WAPI/wapi.py
@@ -0,0 +1,48 @@
+from starlette.applications import Starlette
+from starlette.endpoints import WebSocketEndpoint
+from starlette.routing import WebSocketRoute
+from starlette.websockets import WebSocket
+from pydantic import BaseModel
+
+class WSEndpoint(WebSocketEndpoint):
+    encoding = 'json'
+    
+    async def on_connect(self, websocket):
+        await websocket.accept()
+
+    async def on_receive(self, websocket, data):
+        try:
+            decoded, func = WAPI.decode_incoming(data)
+        except:
+            await websocket.send_json({"error":"invalid JSON"})
+            return -1
+        
+        await func(websocket, decoded)
+
+    async def on_disconnect(self, websocket, close_code):
+        pass
+
+class WAPI(Starlette):
+
+    mapping_class = {}
+
+
+    def decode_incoming(data: dict):
+        for model in WAPI.mapping_class:
+            try:
+                decoded = model(**data)
+                return decoded, WAPI.mapping_class[model]
+            except:
+                print("error")
+
+
+    def __init__(self,endpoint: str):
+        super().__init__(routes=[WebSocketRoute(endpoint, WSEndpoint)])
+    
+    def parse(self, incoming_model: BaseModel):
+        def Inner(func):
+            self.mapping_class[incoming_model] = func
+            def wrapper(*args, **kwargs):
+                func(*args, **kwargs)
+            return wrapper
+        return Inner
---
+++ example/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from ..WAPI.wapi import WAPI, WebSocket
+from pydantic import BaseModel
+
+app = WAPI(endpoint="/ws")
+
+class CM(BaseModel):
+    counter: int
+
+class SD(BaseModel):
+    name: str
+
+@app.parse(CM)
+async def parse_CM(websocket: WebSocket, data: CM):
+    print("RECEIVED CM : ", data)
+    await websocket.send_json(data.dict())
+
+@app.parse(SD)
+async def parse_SD(websocket: WebSocket, data: CM):
+    print("RECEIVED SD : ", data)
+    await websocket.send_json(data.dict())
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
